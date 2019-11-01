768f962f80abcd5c3fe267d053e7ac494f89c49c
ryanbieber <ryan.bieber@protonmail.com>
2019-10-31 21:36:40 -0500

    added

--- R/par_catch_all.R
+++ R/par_catch_all.R
@@ -5,8 +5,9 @@ library(forecastHybrid)
 library(dplyr)
 library(prophet)
 library(lubridate)
+library(dlm)
 
-par_time_series_catch <- function(x, startDate = NULL, freq = "month", steps = 3, num.cores = 2, OutOfSample = FALSE, a.a.args = list(NULL),
+par_time_series_catch <- function(x, freq = "month", steps = 3, dlmPoly = 2, dlmSeas = 12, num.cores = 2, a.a.args = list(NULL),
                                   ets.args = list(NULL), tbats.args = list(NULL), prophet.args = list(NULL), n.n.args = list(NULL)){
   auto_args <- list(max.p = 5, max.q = 5, max.P = 2,
                     max.Q = 2, max.order = 5, max.d = 2, max.D = 1, start.p = 2,
@@ -31,16 +32,12 @@ par_time_series_catch <- function(x, startDate = NULL, freq = "month", steps = 3
   tbats_args <- modifyList(tbats_args, tbats.args)
   prophet_args <- modifyList(prophet_args, prophet.args)
   n_n_args <- modifyList(n_n_args, n.n.args)
-  startDate = as.Date(startDate)
+  startDate = as.Date(format(date_decimal(head(zoo::index(x[[1]]),1)), "%Y-%m-%d"))
 
   if (class(x)!="list"){
     print("Put the data into a list and retry dummy")
     model_list <- NULL
-    break()}
-  if (OutOfSample == FALSE & is.null(startDate)){
-        print("Need to have dates to proceed")
-        break()}
-  else if (OutOfSample == FALSE){
+    break()} else {
       x <- lapply(x, window, start = decimal_date(startDate))
       cl <- makeCluster(getOption("cl.cores", num.cores))
       par_auto <- parLapply(cl,x,auto.arima, max.p = auto_args$max.p, max.q = auto_args$max.q, max.P = auto_args$max.P,
@@ -54,110 +51,96 @@ par_time_series_catch <- function(x, startDate = NULL, freq = "month", steps = 3
                              seasonal.periods = tbats_args$seasonal.periods, use.arma.errors = tbats_args$use.arma.errors)
       par_hybrid <- parLapply(cl, x, hybridModel,  a.args = auto_args, e.args = ets_args, t.args = tbats_args, n.args = n_n_args)
       par_hybrid_in <- parLapply(cl, x, hybridModel, weights = "insample.errors",  a.args = auto_args, e.args = ets_args, t.args = tbats_args, n.args = n_n_args)
-      date <- seq(ymd(startDate), ymd(format(date_decimal(tail(zoo::index(x[[1]]),1)), "%Y-%m-%d")), by = freq)
+      date <- seq(ymd(format(date_decimal(head(zoo::index(x[[1]]),1)), "%Y-%m-%d")), ymd(format(date_decimal(tail(zoo::index(x[[1]]),1)), "%Y-%m-%d")), by = freq)
       prophet_setup <- lapply(x, as.data.frame)
       prophet_setup <- lapply(prophet_setup, cbind, ds=date)
       prophet_setup <- lapply(prophet_setup, rename, y=x)
       par_prophet <- lapply(prophet_setup, prophet, growth = prophet_args$growth, changepoints = prophet_args$changepoints, n.changepoints = prophet_args$n.changepoints,
                             changepoint.range = prophet_args$changepoint.range, yearly.seasonality = prophet_args$yearly.seasonality, weekly.seasonality = prophet_args$weekly.seasonality,
                             daily.seasonality = prophet_args$daily.seasonality, holidays = prophet_args$holidays, seasonality.mode = prophet_args$seasonality.mode)
+      par_dlm <- parLapply(cl, x, dlmPara, dlmPoly=dlmPoly, dlmSeas=dlmSeas, steps = steps, freq = freq)
+      model_list <- c(par_auto, par_ets, par_tbats, par_hybrid, par_hybrid_in, par_prophet, par_dlm)
       stopCluster(cl)
-      model_list <- c(par_auto, par_ets, par_tbats, par_hybrid, par_hybrid_in, par_prophet)
-    } else {
-      if (OutOfSample == TRUE & is.null(startDate)){
-        print("You need to have a starting date ")
-        break()
-      }
-      x <- lapply(x, window, start = decimal_date(startDate))
-      cl <- makeCluster(getOption("cl.cores", num.cores))
-      par_auto <- parLapply(cl,x,auto.arima, max.p = auto_args$max.p, max.q = auto_args$max.q, max.P = auto_args$max.P,
-                            max.Q = auto_args$max.Q, max.order = auto_args$max.order, max.d = auto_args$max.d, max.D = auto_args$max.D,
-                            start.p = auto_args$start.p, start.q = auto_args$start.q, start.P = auto_args$start.P, start.Q = auto_args$start.Q,
-                            stationary = auto_args$stationary, seasonal = auto_args$seasonal, nmodels = auto_args$nmodels)
-      par_ets <- parLapply(cl, x, ets, model = ets_args$model, damped = ets_args$damped, alpha = ets_args$alpha, beta = ets_args$beta,
-                           gamma = ets_args$gamma, phi = ets_args$phi, additive.only = ets_args$additive.only, lambda = ets_args$lambda,
-                           biasadj = ets_args$biasadj)
-      par_tbats <- parLapply(cl, x, tbats, use.box.cox = tbats_args$use.box.cox, use.trend = tbats_args$use.trend, use.damped.trend = tbats_args$use.damped.trend,
-                             seasonal.periods = tbats_args$seasonal.periods, use.arma.errors = tbats_args$use.arma.errors)
-      par_hybrid <- parLapply(cl, x, hybridModel, a.args = auto_args, e.args = ets_args, t.args = tbats_args, n.args = n_n_args)
-      par_hybrid_in <- parLapply(cl, x, hybridModel, weights = "insample.errors", a.args = auto_args, e.args = ets_args, t.args = tbats_args, n.args = n_n_args)
-      date <- seq(ymd(startDate), ymd(format(date_decimal(tail(zoo::index(x[[1]]),1)), "%Y-%m-%d")), by = freq)
-      prophet_setup <- parLapply(cl, x, as.data.frame)
-      prophet_setup <- parLapply(cl, prophet_setup, cbind, ds=date)
-      prophet_setup <- parLapply(cl, prophet_setup, rename, y=x)
-      par_prophet <- parLapply(cl, prophet_setup, prophet, growth = prophet_args$growth, changepoints = prophet_args$changepoints, n.changepoints = prophet_args$n.changepoints,
-                            changepoint.range = prophet_args$changepoint.range, yearly.seasonality = prophet_args$yearly.seasonality, weekly.seasonality = prophet_args$weekly.seasonality,
-                            daily.seasonality = prophet_args$daily.seasonality, holidays = prophet_args$holidays, seasonality.mode = prophet_args$seasonality.mode)
-      stopCluster(cl)
-      model_list <- c(par_auto, par_ets, par_tbats, par_hybrid, par_hybrid_in, par_prophet)
     }
+
   return(model_list)
 }
 
-hundo <- replicate(5, list(ldeaths))
-test <- par_time_series_catch(hundo, num.cores = 12, startDate = "1974-01-01")
 
+dlmPara <- function ( x, dlmPoly = dlmPoly, dlmSeas = dlmSeas, steps = steps , freq = freq){
+  model.build <- function(p) {
+    return(
+      dlm::dlmModPoly(dlmPoly, dV=p[1], dW=p[2:3]) +
+        dlm::dlmModSeas(dlmSeas, dV=p[4])
+    )
+  }
+  model.mle <- dlm::dlmMLE(x, parm=c(0.1, 0, 1, 1), build=model.build)
+  model.fit <- model.build(model.mle$par)
+  if (freq == "month"){
+    time <- 12
+  } else if (freq == "day") {
+    time <- 365
+  } else if (freq == "week") {
+    time <- 52
+  } else if (freq == "quarter") {
+    time <- 4
+  } else { time <- 1}
+  model.filtered <- dlm::dlmFilter(x, model.fit)
+  model.smoothed <- dlm::dlmSmooth(x, model.fit)
+  model.forecast <- dlm::dlmForecast(model.filtered, nAhead=steps)
+  xf <- seq(max(zoo::index(x)), max(zoo::index(x))+steps/time, 1/time)
+  xf <- xf[(-1)]
+  aa <- model.forecast$a[,-1]*(-1)
+  aa <- cbind(model.forecast$a[,1], aa)
+  a <- drop(model.forecast$a%*%t(dlm::FF(model.fit)))
+  a <- c(x,a)
+  class(a) <- "dlm"
+  return(a)
+}
 
-extract_model_fit_forecast <- function(x, steps = 0, xreg = NULL, freq = "month"){
+extract_model_fit_forecast <- function(x, steps = 3, xreg = NULL, freq = "month"){
   if (any(class(x) %in% c("tbats"))){
     tbats_fit <- as.numeric(fitted.values(x))
-    if (steps>0){
-      tbats_fcast <- forecast::forecast(x, h = steps)
-      tbats_fcast <- as.numeric(tbats_fcast$mean)
-      final <- c(tbats_fit, tbats_fcast)
-    } else {
-      final <- tbats_fit
-    }
-
+    tbats_fcast <- forecast::forecast(x, h = steps)
+    tbats_fcast <- as.numeric(tbats_fcast$mean)
+    final <- c(tbats_fit, tbats_fcast)
   } else if(any(class(x) %in% c("Arima"))){
     arima_fit <- fitted.values(x)
-    if (steps>0){
-      arima_fcast <- forecast::forecast(x, h = steps, xreg = xreg)
-      arima_fcast <- as.numeric(arima_fcast$mean)
-      final <- c(arima_fit, arima_fcast)
-    } else {
-      final <- arima_fit
-    }
+    arima_fcast <- forecast::forecast(x, h = steps, xreg = xreg)
+    arima_fcast <- as.numeric(arima_fcast$mean)
+    final <- c(arima_fit, arima_fcast)
   } else if(any(class(x) %in% c("ets"))){
     ets_fit <- fitted.values(x)
-    if (steps>0){
-      ets_fcast <- forecast::forecast(x, h = steps)
-      ets_fcast <- as.numeric(ets_fcast$mean)
-      final <- c(ets_fit, ets_fcast)
-    } else {
-      final <- ets_fit
-    }
-  } else if(any(class(x) %in% c("hybridModel"))){
-    hybrid_fit <- fitted.values(x)
-    if (steps>0){
-      hybrid_fcast <- forecast::forecast(x, h = steps, xreg = xreg)
-      hybrid_fcast <- as.numeric(hybrid_fcast$mean)
-      final <- c(hybrid_fit, hybrid_fcast)
-    } else {
-      final <- hybrid_fit
-    }
-    } else if(any(class(x) %in% c("prophet"))){
-      if (steps == 0){
-        steps <- 1
-        future <- prophet::make_future_dataframe(x, period = steps, freq = freq)
-        forecast <- predict(x, future)
-        final <- head(forecast$yhat,-1)
-      } else {
-        future <- prophet::make_future_dataframe(x, period = steps, freq = freq)
-        forecast <- predict(x, future)
-        final <- forecast$yhat
-      }
-
+    ets_fcast <- forecast::forecast(x, h = steps)
+    ets_fcast <- as.numeric(ets_fcast$mean)
+    final <- c(ets_fit, ets_fcast)
+    ## this forecasting doesnt work in parallel for some reason but it does with lapply need to investigate further
+  # } else if(any(class(x) %in% c("hybridModel"))){
+  #   hybrid_fit <- fitted.values(x)
+  #   hybrid_fcast <- forecast::forecast(x, h = steps, xreg = xreg)
+  #   hybrid_fcast <- as.numeric(hybrid_fcast$mean)
+  #   final <- c(hybrid_fit, hybrid_fcast)
+  } else if(any(class(x) %in% c("prophet"))){
+    future <- prophet::make_future_dataframe(x, period = steps, freq = freq)
+    forecast <- predict(x, future)
+    final <- forecast$yhat
+  } else if(any(class(x) %in% c("dlm"))){
+    final <- x
   } else {
-      final <- NULL
-    }
+    final <- NULL
+  }
   return(final)
 }
 
-system.time( lapply(test, extract_model_fit_forecast, steps = 1))
 
+hundo <- replicate(5, list(ldeaths))
+test <- par_time_series_catch(hundo, num.cores = 12)
 cl <- makeCluster(getOption("cl.cores", 12))
-system.time(parLapply(cl, test, extract_model_fit_forecast, steps = 1))
+test_fit <- parLapply(cl, test, extract_model_fit_forecast)
 stopCluster(cl)
 
 
+hybrid_fit <- fitted.values(test[[22]])
+hybrid_fcast <- forecast::forecast(test[[22]], h = 3, xreg = NULL)
+hybrid_fcast <- hybrid_fcast$mean
+final <- c(hybrid_fit, hybrid_fcast)
