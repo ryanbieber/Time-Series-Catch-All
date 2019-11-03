7fc994e3e54f62bac3aafdf5d3e0811197693d4e
ryanbieber <ryan.bieber@protonmail.com>
2019-11-03 07:28:58 -0600

    changed tests

--- tests/testthat/test-time_series_catch.R
+++ tests/testthat/test-time_series_catch.R
@@ -1,37 +1,9 @@
 test_that("The functions are all running correctly", {
 
   # building the test data
-          Date <- seq(as.Date("1974-1-01"), as.Date("1979-12-01"), by="months")
-          df <- data.frame(ldeaths, mdeaths, fdeaths)
-          data <- cbind(Date, df)
-          f=12
-          h=3
-          trainStart = c(1974,1)
-          trainEnd = c(1979,9)
-          testStart = c(1979, 10)
-          n=12
-  # main testing function
-          expect_warning( DF <- time_series_catch(data, f, h, trainStart, trainEnd, testStart, n, OutOfSample = FALSE))
-  # errors
-          DFError <- error_values(DF, h)
-  # best forecast by error
-          DFMAPE <- best_forecast_by_MAPE(DF, data,  f, DFError, trainStart)
-          DFMSE <- best_forecast_by_MSE(DF, data,  f, DFError, trainStart)
-          DFRMSE <- best_forecast_by_RMSE(DF, data,  f, DFError, trainStart)
-  # name of the best model
-          MAPE <- name_of_best_model(data, DFError, x=1)
-          RMSE <- name_of_best_model(data, DFError, x=2)
-          MSE <- name_of_best_model(data, DFError, x=3)
-  # output to csv
-          csv_forecast_values(data, DFMAPE, h, MAPE, DFError, x=1)
-          csv_forecast_values(data, DFRMSE, h, RMSE, DFError, x=2)
-          csv_forecast_values(data, DFMSE, h, MSE, DFError, x=3)
-  # fast catch_all
-          trainStart = c(1974,1)
-          trainEnd = c(1979,12)
-          testStart = c(1980, 1)
-         expect_warning( DFFast <- fast_time_series_catch_all(data, f, h, trainStart, trainEnd, testStart, n, MAPE, OutOfSample = TRUE))
-
+         test <- replicate(5, list(ldeaths))
+         expect_error(test_model <- par_time_series_catch(test, num.cores = 12))
+         test_forecast <- extract_model_fit_forecast(test_model, num.cores = 12)
           })
 
 
