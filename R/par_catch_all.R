badf9e034a5f90dc45d3800614e8c531b0570f86
ryanbieber <ryan.bieber@protonmail.com>
2019-11-03 09:32:30 -0600

    fixed

--- R/par_catch_all.R
+++ R/par_catch_all.R
@@ -17,9 +17,10 @@
 #' @return list of models for eacg ts
 #' @export
 #'
-#' @examples test <- replicate(5, list(ldeaths))
+#' @examples \dontrun{
+#' test <- replicate(5, list(ldeaths))
 #' test_model <- par_time_series_catch(test, num.cores = 12)
-#' test_forecast <- extract_model_fit_forecast(test_model, num.cores = 12)
+#' test_forecast <- extract_model_fit_forecast(test_model, num.cores = 12)}
 #'
 #'
 #'
@@ -43,41 +44,42 @@ par_time_series_catch <- function(x, freq = "month", steps = 3, dlmPoly = 2, dlm
                        seasonality.mode = "additive")
   n_n_args <- list(P = 1, repeats = 20, xreg = NULL, lambda = NULL,
                    model = NULL, subset = NULL, scale.inputs = TRUE)
-  auto_args <- modifyList(auto_args, a.a.args)
-  ets_args <- modifyList(ets_args, ets.args)
-  tbats_args <- modifyList(tbats_args, tbats.args)
-  prophet_args <- modifyList(prophet_args, prophet.args)
-  n_n_args <- modifyList(n_n_args, n.n.args)
-  startDate = as.Date(format(date_decimal(head(zoo::index(x[[1]]),1)), "%Y-%m-%d"))
+  auto_args <- utils::modifyList(auto_args, a.a.args)
+  ets_args <- utils::modifyList(ets_args, ets.args)
+  tbats_args <- utils::modifyList(tbats_args, tbats.args)
+  prophet_args <- utils::modifyList(prophet_args, prophet.args)
+  n_n_args <- utils::modifyList(n_n_args, n.n.args)
+  startDate = zoo::as.Date(format(lubridate::date_decimal(utils::head(zoo::index(x[[1]]),1)), "%Y-%m-%d"))
 
   if (class(x)!="list"){
     print("Put the data into a list and retry dummy")
     model_list <- NULL
     stop()
     } else {
-      x <- lapply(x, window, start = decimal_date(startDate))
-      cl <- makeCluster(getOption("cl.cores", num.cores))
-      par_auto <- parLapply(cl,x,auto.arima, max.p = auto_args$max.p, max.q = auto_args$max.q, max.P = auto_args$max.P,
+      x <- lapply(x, stats::window, start = lubridate::decimal_date(startDate))
+      cl <- parallel::makeCluster(getOption("cl.cores", num.cores))
+      par_auto <- parallel::parLapply(cl,x,forecast::auto.arima, max.p = auto_args$max.p, max.q = auto_args$max.q, max.P = auto_args$max.P,
                             max.Q = auto_args$max.Q, max.order = auto_args$max.order, max.d = auto_args$max.d, max.D = auto_args$max.D,
                             start.p = auto_args$start.p, start.q = auto_args$start.q, start.P = auto_args$start.P, start.Q = auto_args$start.Q,
                             stationary = auto_args$stationary, seasonal = auto_args$seasonal, nmodels = auto_args$nmodels)
-      par_ets <- parLapply(cl, x, ets, model = ets_args$model, damped = ets_args$damped, alpha = ets_args$alpha, beta = ets_args$beta,
+      par_ets <- parallel::parLapply(cl, x, forecast::ets, model = ets_args$model, damped = ets_args$damped, alpha = ets_args$alpha, beta = ets_args$beta,
                            gamma = ets_args$gamma, phi = ets_args$phi, additive.only = ets_args$additive.only, lambda = ets_args$lambda,
                            biasadj = ets_args$biasadj)
-      par_tbats <- parLapply(cl, x, tbats, use.box.cox = tbats_args$use.box.cox, use.trend = tbats_args$use.trend, use.damped.trend = tbats_args$use.damped.trend,
+      par_tbats <- parallel::parLapply(cl, x, forecast::tbats, use.box.cox = tbats_args$use.box.cox, use.trend = tbats_args$use.trend, use.damped.trend = tbats_args$use.damped.trend,
                              seasonal.periods = tbats_args$seasonal.periods, use.arma.errors = tbats_args$use.arma.errors)
-      par_hybrid <- parLapply(cl, x, hybridModel,  a.args = auto_args, e.args = ets_args, t.args = tbats_args, n.args = n_n_args)
-      par_hybrid_in <- parLapply(cl, x, hybridModel, weights = "insample.errors",  a.args = auto_args, e.args = ets_args, t.args = tbats_args, n.args = n_n_args)
-      date <- seq(ymd(format(date_decimal(head(zoo::index(x[[1]]),1)), "%Y-%m-%d")), ymd(format(date_decimal(tail(zoo::index(x[[1]]),1)), "%Y-%m-%d")), by = freq)
+      par_hybrid <- parallel::parLapply(cl, x, forecastHybrid::hybridModel,  a.args = auto_args, e.args = ets_args, t.args = tbats_args, n.args = n_n_args)
+      par_hybrid_in <- parallel::parLapply(cl, x, forecastHybrid::hybridModel, weights = "insample.errors",  a.args = auto_args, e.args = ets_args, t.args = tbats_args, n.args = n_n_args)
+      date <- seq(lubridate::ymd(format(lubridate::date_decimal(utils::head(zoo::index(x[[1]]),1)), "%Y-%m-%d")),
+                  lubridate::ymd(format(lubridate::date_decimal(utils::tail(zoo::index(x[[1]]),1)), "%Y-%m-%d")), by = freq)
       prophet_setup <- lapply(x, as.data.frame)
       prophet_setup <- lapply(prophet_setup, cbind, ds = date)
-      prophet_setup <- lapply(prophet_setup, rename, y = x)
-      par_prophet <- lapply(prophet_setup, prophet, growth = prophet_args$growth, changepoints = prophet_args$changepoints, n.changepoints = prophet_args$n.changepoints,
+      prophet_setup <- lapply(prophet_setup, dplyr::rename, y = x)
+      par_prophet <- lapply(prophet_setup, prophet::prophet, growth = prophet_args$growth, changepoints = prophet_args$changepoints, n.changepoints = prophet_args$n.changepoints,
                             changepoint.range = prophet_args$changepoint.range, yearly.seasonality = prophet_args$yearly.seasonality, weekly.seasonality = prophet_args$weekly.seasonality,
                             daily.seasonality = prophet_args$daily.seasonality, holidays = prophet_args$holidays, seasonality.mode = prophet_args$seasonality.mode)
-      par_dlm <- parLapply(cl, x, dlmPara, dlmPoly = dlmPoly, dlmSeas = dlmSeas, steps = steps, freq = freq)
+      par_dlm <- parallel::parLapply(cl, x, dlmPara, dlmPoly = dlmPoly, dlmSeas = dlmSeas, steps = steps, freq = freq)
       model_list <- c(par_auto, par_ets, par_tbats, par_hybrid, par_hybrid_in, par_prophet, par_dlm)
-      stopCluster(cl)
+      parallel::stopCluster(cl)
     }
 
   return(model_list)
@@ -95,9 +97,11 @@ par_time_series_catch <- function(x, freq = "month", steps = 3, dlmPoly = 2, dlm
 #' @return array of fitted + forecast
 #' @export
 #'
-#' @examples test <- replicate(5, list(ldeaths))
+#' @examples \dontrun{
+#' test <- replicate(5, list(ldeaths))
 #' test_model <- par_time_series_catch(test, num.cores = 12)
-#' test_forecast <- extract_model_fit_forecast(test_model, num.cores = 12)
+#' test_forecast <- extract_model_fit_forecast(test_model, num.cores = 12)}
+
 dlmPara <- function ( x, dlmPoly = dlmPoly, dlmSeas = dlmSeas, steps = steps , freq = freq){
   model.build <- function(p) {
     return(
@@ -140,9 +144,11 @@ dlmPara <- function ( x, dlmPoly = dlmPoly, dlmSeas = dlmSeas, steps = steps , f
 #' @return list of numeric arrays with forecasts appended
 #' @export
 #'
-#' @examples  test <- replicate(5, list(ldeaths))
+#' @examples \dontrun{
+#' test <- replicate(5, list(ldeaths))
 #' test_model <- par_time_series_catch(test, num.cores = 12)
-#' test_forecast <- extract_model_fit_forecast(test_model, num.cores = 12)
+#' test_forecast <- extract_model_fit_forecast(test_model, num.cores = 12)}
+#'
 extract_model_fit_forecast <- function(x, steps = 3, xreg = NULL, freq = "month", num.cores = 2){
   a = list()
   b = list()
@@ -163,17 +169,17 @@ extract_model_fit_forecast <- function(x, steps = 3, xreg = NULL, freq = "month"
     c[[i]] <- x[[i]]
   }
   c <- c[lengths(c) != 0]
-  cl <- makeCluster(getOption("cl.cores", num.cores))
-  final_forecast <- parLapply(cl, a, forecast, h=steps, xreg=xreg)
-  final_hybrid <- lapply(hybrid, forecast, h=steps, xreg=xreg)
+  cl <- parallel::makeCluster(getOption("cl.cores", num.cores))
+  final_forecast <- parallel::parLapply(cl, a, forecast::forecast, h=steps, xreg=xreg)
+  final_hybrid <- lapply(hybrid, forecast::forecast, h=steps, xreg=xreg)
   prophet_forecast <- function(x, period = 3, freq = "month"){
     future <- prophet::make_future_dataframe(x, period, freq)
-    forecast <- predict(x, future)
-    final <- forecast$yhat
+    forecastp <- stats::predict(x, future)
+    final <- forecastp$yhat
     return(final)
   }
-  final_proph <- parLapply(cl, b, prophet_forecast, period = steps, freq = freq)
-  stopCluster(cl)
+  final_proph <- parallel::parLapply(cl, b, prophet_forecast, period = steps, freq = freq)
+  parallel::stopCluster(cl)
   final_dlm <- c
   forecast_combine <- c(final_forecast, final_hybrid)
   final_reg <- lapply(forecast_combine, function(x){c(x$fitted, x$mean)})
--- tests/testthat/test-time_series_catch.R
+++ tests/testthat/test-time_series_catch.R
@@ -1,9 +1,8 @@
 test_that("The functions are all running correctly", {
-
-  # building the test data
-         test <- replicate(5, list(ldeaths))
-         test_model <- par_time_series_catch(test, num.cores = 12)
-         test_forecast <- extract_model_fit_forecast(test_model, num.cores = 12)
+        library(TimeSeriesCatchAll)
+         test <- replicate(2, list(ldeaths))
+         test_model <- par_time_series_catch(test)
+         test_forecast <- extract_model_fit_forecast(test_model)
           })
 
 
